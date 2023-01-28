9c4299183b943ebeb303ed3e60f5c246c6315626
Alif Imtiaj <lolalif412@gmail.com>
2023-01-28 15:42:14 +0600

    escape sequence

---
+++ Code/java02_escape.java
@@ -0,0 +1,24 @@
+/*
+ * Escape sequences:
+ * 
+ * \b = backspace
+ * \t = tab
+ * \n = new line
+ * \r = carriage return
+ * \" = double quote (show ' " ')
+ * \' = single quote
+ * \\ = backslash 
+ * 
+ */
+
+
+public class java02_escape {
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+        System.out.print("\nMy name is Alif Imtiaj"); // \n enter a new line then print the strings
+        System.out.print("\n One lett\ber missing");
+        System.out.print("\n\'a\' is a character");
+        System.out.print("\n \"A string\" \t-> This is a string"); // \t add a tab
+
+    }
+}
