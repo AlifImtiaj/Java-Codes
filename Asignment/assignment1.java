ea7a2d409023284659371f98121f1531f8991090
Alif Imtiaj <lolalif412@gmail.com>
2023-01-28 15:30:00 +0600

    Assignment-1
    Completed assignment-1 where I had to print some text.

---
+++ Asignment/assignment1.java
@@ -0,0 +1,24 @@
+
+/*
+ * Assignment-1
+ * create a class called Product
+ * create a main method
+ * print the following data in main method
+        id: 101,
+        title: iphone15,
+        price: 1895 euros,
+        description: perfect product with best image quality,
+        category: phone,
+ */
+
+
+public class assignment1 {
+    public static void main(String[] args)
+    {
+        System.out.print("Id: 101,");
+        System.out.println("Title: iphone12,");
+        System.out.println("Price: 1250 euros,");
+        System.out.println("Description: perfect product with best image quality,");
+        System.out.println("Category: phone");
+    }
+}
