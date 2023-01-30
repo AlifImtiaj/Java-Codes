db702e7b5f1d6d4a9b05d61fa3f5639a58d69744
Alif Imtiaj <lolalif412@gmail.com>
2023-01-30 22:49:44 +0600

    Update variables1.java

--- Code/variables1.java
+++ Code/variables1.java
@@ -1,8 +1,10 @@
 public class variables1 {
     // variable is one kind of name which used to store value in it.
     // num1 = 20, here num1 is a variable
-    // we can use (A....Z, a...z), (0,1,...9,), '_', '$';
+    // we can only use (A....Z, a...z), (0,1,...9,), '_', '$';
     // we can't start any variables with a number as first letter
     // we can't use any keyword as variable
     // we can't put any space in between the name of variable
+
+    // some examples : num1, num2, alif, _Imtiaj, role_number, value$
 }
