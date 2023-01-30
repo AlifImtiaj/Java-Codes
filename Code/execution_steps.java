23a64bb3d5e203392fe3ca035eb35fc0019fb90e
Alif Imtiaj <lolalif412@gmail.com>
2023-01-30 22:42:56 +0600

    Update execution_steps.java

--- Code/execution_steps.java
+++ Code/execution_steps.java
@@ -1,3 +1,12 @@
 public class execution_steps {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        // Execution steps are 1. Edit, 2. Compile, 3. Load, 4. Verify, 5.Execute
+        /*
+         * We edit source code(.java) with text editor, the source code then
+         * read by java compiler. Then java compiler produces Byte code(.class),
+         * then its intrepeted by Java virtual machine, which results in program execution
+         */
+    }
 }
