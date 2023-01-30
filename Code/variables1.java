2f547578c7030c1fad264a06dabec833cb34602d
Alif Imtiaj <lolalif412@gmail.com>
2023-01-30 22:48:19 +0600

    Create variable1.java

---
+++ Code/variables1.java
@@ -0,0 +1,8 @@
+public class variables1 {
+    // variable is one kind of name which used to store value in it.
+    // num1 = 20, here num1 is a variable
+    // we can use (A....Z, a...z), (0,1,...9,), '_', '$';
+    // we can't start any variables with a number as first letter
+    // we can't use any keyword as variable
+    // we can't put any space in between the name of variable
+}
