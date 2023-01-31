f42b6ed8407ed7cba1706b507544a2c5e4351a0b
Alif Imtiaj <lolalif412@gmail.com>
2023-01-31 16:06:59 +0600

    Update variables1.java

--- Code/variables1.java
+++ Code/variables1.java
@@ -7,4 +7,34 @@ public class variables1 {
     // we can't put any space in between the name of variable
 
     // some examples : num1, num2, alif, _Imtiaj, role_number, value$
+
+    /*
+     * data types
+     * 
+     * which type of data are going to be stored on variables is called data type.
+     * there are mainly two types of data types, 1. Primitive , 2. Non-primitive 
+     * 
+     * -- Primitive --
+     * {
+     * Primitive are 2 types.
+     * 1 -> boolean 2 -> Numeric
+     * 
+     * Numeric are 2 types.
+     * 1 -> Character 2 -> Integral
+     * 
+     * Integral are 2 types.
+     * 
+     * 1 -> Integer 2 -> Floating
+     * 
+     * Integer are 4 types.
+     * 1 -> byte 2 -> short 3 -> int 4 -> long
+     * 
+     * Floating-point are 2 types.
+     * 
+     * 1 -> float 2 -> Double
+     * }
+     * 
+     * -- Non-premitive --
+     * Non-primitve data types are {string, Array etc.}
+     */
 }
